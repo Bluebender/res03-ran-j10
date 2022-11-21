1ecc00c40be8631f84329fe47403cbfb446fbea6
Bluebender <olliviev@gmail.com>
2022-11-21 15:12:48 +0000

    Quatrième sauvegarde

--- main.js
+++ main.js
@@ -4,25 +4,35 @@ let section3 = document.getElementById("section3");
 let bouton1 = document.getElementById("bouton1");
 let bouton2 = document.getElementById("bouton2");
 let bouton3 = document.getElementById("bouton3");
-
+let input = document.querySelectorAll("section:first-of-type input");
+let input2 = document.querySelectorAll("section:nth-of-type(2) input");
+let input3 = document.querySelectorAll("section:last-of-type input");
 
 bouton1.addEventListener("click", function(){
     section1.classList.add("passive");
-    section1.classList.remove("active");
-    section2.classList.add("active");
     section2.classList.remove("passive");
+    section3.classList.add("passive");
+    for (let i=0; i<input.length; i++){
+        input[i].setAttribute("disabled", "true");
+    }
+    
 });
 
 bouton2.addEventListener("click", function(){
+    section1.classList.add("passive");
     section2.classList.add("passive");
-    section2.classList.remove("active");
-    section3.classList.add("active");
     section3.classList.remove("passive");
+    for (let i=0; i<input2.length; i++){
+        input2[i].setAttribute("disabled", "true");
+    }
 });
 
 bouton3.addEventListener("click", function(){
-    section3.classList.add("passive");
-    section3.classList.remove("active");
-    section1.classList.add("active");
     section1.classList.remove("passive");
+    section2.classList.add("passive");
+    section3.classList.add("passive");
+    for (let i=0; i<input3.length; i++){
+        input3[i].setAttribute("disabled", "true");
+    }
 });
+
